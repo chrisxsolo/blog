4e3a4e57622be07a893fa751964eb1143848d53c
Chris Solorzano <chrisxsolo177@gmail.com>
2023-07-27 14:50:42 -0700

    took out a photo

--- app/blog/page.js
+++ app/blog/page.js
@@ -19,14 +19,14 @@ const blogPosts = [
         type: 'text',
         text: "This shoot was a spontaneous idea and it became one of my favorites. One of the best things about photography is bringing to life a vision that you have in the moment. The day before, I had the idea of shooting with golden grass and having my model hold my x100v. Ive seen it done before by marco and had inspo images ready. I had wanted to do a shoot like this for a long time. Now that it was finally summer, it was perfect for that. It was sunny and the grass was golden. I had shot here before last summer and during the winter. I had seen this place in almost all seasons and it looked the best in summer. I posted on my story the day before asking who wanted to shoot in Palo Alto. I was lucky that Nicole was available. This was my fourth time taking pics with her. Nicole had the perfect personality for the vibe that I wanted. Nicole is always fun to shoot with and I genuinely enjoy each shoot with her. She made it easy to get the shots that I wanted and more. I took some video in the last 20 minutes of the shoot. I always struggle with doing video and photo in the same session. It’s something that I need to work on. I get super frustrated when I mess up with my video settings and waste precious time when the conditions are perfect. I was rushing and fumbling with my gear trying to get my gimbal calibrated and ready while the sun was setting over the horizon. By the time everything was ready, the sun had already set which was a bummer. I had been wanting to do more video portraits with backlighting but once again, I wasn’t good enough at timing everything right. We still shot some video but I felt like I could have done much better. At the end of the day, I got the photos that I wanted. I have always been a photo-first creative so when the photos turn out fine and the video turns out not how I expected, I’m still kind of satisfied but just disappointed in myself. Especially now that I want to get more into video portraits. I’m still learning and practicing but it feels terrible when you don’t meet your own expectations. We ended the shoot with getting boba and hanging out. All in all, this shoot was a success and makes me look forward to more shoots with Nicole.",
       },
-      {
-        type: 'image',
-        imageUrl: "/DSC00624.jpg",
-      },
-      {
-        type: 'text',
-        text: "",
-      },
+    //   {
+    //     type: 'image',
+    //     imageUrl: "/DSC00624.jpg",
+    //   },
+    //   {
+    //     type: 'text',
+    //     text: "",
+    //   },
       // Add more content sections (image and text pairs) as needed
     ],
   },
@@ -64,50 +64,68 @@ export default function Blog() {
         [index]: !prevState[index],
       }));
     };
-  
     return (
         <main>
-         <div className={styles['navbar-container']}>
-        <NavBar />
-      </div>
-      <div className={styles.title}>
-        <h2>Thoughts.</h2>
-      </div>
-        {blogPosts.map((post, index) => (
-          <div key={index} className={styles.blogPost}>
-            <div className={styles['blog-title']}>
-              <h3>{post.title}</h3>
-              <p className={styles['timestamp']}>{post.timestamp}</p>
-            </div>
-            <div className={styles['blog-content']}>
-              {post.content.map((section, sectionIndex) => (
-                <div key={sectionIndex} className={styles['content-section']}>
-                  {section.type === 'image' && (
-                    <div className={styles['blog-img']}>
-                      {/* Use the next/image component to display the image */}
-                      <Image src={section.imageUrl} alt={`Image ${sectionIndex + 1}`} width={400} height={600} />
-                    </div>
-                  )}
-                  {section.type === 'text' && (
-                    <div className={styles['content-text']}>
-                      {/* Show only a limited amount of text or full text based on the expanded state */}
-                      <p>
-                        {expandedPosts[index] ? section.text : section.text.slice(0, 400)}
-                      </p>
-                      {section.text.length > 200 && (
-                        <div className={styles['read-more-container']}>
-                          <button onClick={() => handleReadMoreClick(index)} className={styles['read-more']}>
-                            {expandedPosts[index] ? 'Collapse' : 'Read More'}
-                          </button>
-                        </div>
-                      )}
-                    </div>
-                  )}
-                </div>
-              ))}
-            </div>
+          {/* Render the NavBar component */}
+          <div className={styles['navbar-container']}>
+            <NavBar />
           </div>
-        ))}
-      </main>
-    );
-  }
+      
+          {/* Display the title */}
+          <div className={styles.title}>
+            <h2>Thoughts.</h2>
+          </div>
+      
+          {/* Map through the blogPosts array to render each blog post */}
+          {blogPosts.map((post, index) => (
+            // Use the index as the key for the blog post
+            <div key={index} className={styles.blogPost}>
+              {/* Render the blog title and timestamp */}
+              <div className={styles['blog-title']}>
+                <h3>{post.title}</h3>
+                <p className={styles['timestamp']}>{post.timestamp}</p>
+              </div>
+      
+              {/* Render the blog content, which can contain text and/or images */}
+              <div className={styles['blog-content']}>
+                {post.content.map((section, sectionIndex) => (
+                  // Use the sectionIndex as the key for each content section
+                  <div key={sectionIndex} className={styles['content-section']}>
+                    {/* Check if the content section is an image */}
+                    {section.type === 'image' && (
+                      <div className={styles['blog-img']}>
+                        {/* Use the next/image component to display the image */}
+                        <Image src={section.imageUrl} alt={`Image ${sectionIndex + 1}`} width={400} height={600} />
+                      </div>
+                    )}
+      
+                    {/* Check if the content section is text */}
+                    {section.type === 'text' && (
+                      <div className={styles['content-text']}>
+                        {/* Show only a limited amount of text or full text based on the expanded state */}
+                        <p>
+                          {/* If the blog post is expanded, show the full text */}
+                          {/* Otherwise, show the first 400 characters of the text */}
+                          {expandedPosts[index] ? section.text : section.text.slice(0, 400)}
+                        </p>
+      
+                        {/* Check if the text is long enough to require a "Read More" button */}
+                        {section.text.length > 200 && (
+                          <div className={styles['read-more-container']}>
+                            {/* Render the "Read More" button */}
+                            <button onClick={() => handleReadMoreClick(index)} className={styles['read-more']}>
+                              {/* If the blog post is expanded, show "Collapse", otherwise show "Read More" */}
+                              {expandedPosts[index] ? 'Collapse' : 'Read More'}
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </main>
+      );
+    }
