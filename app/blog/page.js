08c8d3482cb14134c99ca3ad58a47ceb026ab87e
Chris Solorzano <chrisxsolo177@gmail.com>
2023-07-26 21:38:45 -0700

    blog part

---
+++ app/blog/page.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import styles from './blog.module.css';
+import NavBar from '../components/NavBar';
+import React from 'react';
+
+
+export default function Blog() {
+  return (
+    <main>
+      <div className={styles['navbar-container']}>
+        <NavBar />
+      </div>
+      <div className={styles.title}>
+        <h2>Thoughts.</h2>
+      </div>
+    </main>
+  );
+}
